00bde7902e7956df77b4aee2e1c15a7cc3c1af95
Ryan R <ryan1991@hotmail.fr>
2022-01-12 16:47:11 +0100

    Correction du menu et d'un bug lié à l'IA

--- src/Menu.py
+++ src/Menu.py
@@ -22,8 +22,7 @@ class Menu:
 
     def start(self):
         """
-        Permet d'afficher le menu dans lequel nous choisissons le mode de jeu.
-        :return: Board
+        Demande au joueur quel menu lancer
         """
 
         table_data = [
@@ -41,11 +40,23 @@ class Menu:
         if(x == "player"):
             self.menu_player()
 
+    def display_player_list(self):
+        """
+        Méthode permettant de donner la liste des joueurs.
+        """
+        table_data = [
+            ['Joueurs']]
+        for k in range(len(self.players)):
+            name = self.players[k].get_name()
+            table_data.append([ str(k) + " : " + name])
+        table = AsciiTable(table_data)
+        print(table.table)
+
     def isNotPlayer(self, choice):
         """
         Permet de tester que le nom du joueur en paramètre correspond bien à celui d'un joueur de la liste des joueurs.
         :param choice: bool
-        :return:
+        :return: bool
         """
         for player in self.players:
             if player.get_name() == choice:
@@ -54,9 +65,9 @@ class Menu:
 
     def get_index_player(self, choice):
         """
-        Méthode permettant d'afficher le nom du joueur.
-        :param choice: Choix du joueur.
-        :return: Nom joueur.
+        Donne l'index dans la liste des joueurs du joueur donné en paramètre
+        :param choice: Nom du joueur
+        :return: Index du joueur
         """
         for k in range(len(self.players)):
             if self.players[k].get_name() == choice:
@@ -67,7 +78,6 @@ class Menu:
 
         """
         Demande au joueur le mode de jeu qu'il veut lancer
-        :return:
         """
 
         table_data = [
@@ -94,36 +104,7 @@ class Menu:
         if(x == "2"):
             self.start_pc_vs_pc()
 
-<<<<<<< Updated upstream
-    def display_player_list(self):
-        """
-        Méthode permettant de donner la liste des joueurs.
-        :return: liste des joueurs.
-        """
-        table_data = [
-            ['Joueurs']]
-        for k in range(len(self.players)):
-            name = self.players[k].get_name()
-            table_data.append([ str(k) + " : " + name])
-        table = AsciiTable(table_data)
-        print(table.table)
-
-    def menu_player(self):
-        """
-        Méthode permettant de créer un nouveau joueur.
-        :return: Liste joueurs avec le nouveau joueur.
-        """
-        self.display_player_list()
-        
-        table_data2 = [
-            ['Que voulez-vous faire ?'],
-            ['Créer un nouveau joueur (add Nom | Supprimer un joueur (delete Nom)']
-            ]
-        table2 = AsciiTable(table_data2)
-        print(table2.table)
-=======
         self.start()
->>>>>>> Stashed changes
 
     def start_human_vs_human(self):
         """
@@ -163,7 +144,6 @@ class Menu:
     def start_human_vs_pc(self):
         """
         Demande que jouent le joueur et le pc et lance une partie en humain contre IA
-        :return:
         """
         self.display_player_list()
 
@@ -181,8 +161,10 @@ class Menu:
         if(choiceWhite == "pc"):
             player1 = Player("IA", True, True)
             player2 = self.players[self.get_index_player(choiceBlack)]
+            player2.set_color(False)
         else:
             player1 = self.players[self.get_index_player(choiceWhite)]
+            player1.set_color(True)
             player2 = Player("IA", False, True)
 
         board = chess.Board()
@@ -193,10 +175,9 @@ class Menu:
 
     def menu_player(self):
         """
-        Demande au joueur ce qu'il veut faire avec la list des joueurs
+        Demande au joueur ce qu'il veut faire avec la liste des joueurs et l'execute
         :return:
         """
-
         self.display_player_list()
         
         table_data2 = [
@@ -217,36 +198,6 @@ class Menu:
             pass
 
         self.start()
-
-    def create_player(self):
-        """
-        Création d'un nouveau joueur en rentrant les informations de ce dernier dans le terminal
-        :return:
-        """
-        table_data = [
-            ['Nouveau joueur !'],
-            ['player add Pseudo Color isPC']
-            ]
-        table = AsciiTable(table_data)
-        print(table.table)
-
-    def modify_player(self):
-        """
-        Modifier les informations d'un joueur
-        :return:
-        """
-        table_data = [
-            ['Modifier joueur !'],
-            ['player modify Pseudo']
-            ]
-        table = AsciiTable(table_data)
-        print(table.table)
-
-    def delete_player(self):
-        """
-        Supprimer un joueur
-        :return:
-        """
 
 ## Menu option
 
