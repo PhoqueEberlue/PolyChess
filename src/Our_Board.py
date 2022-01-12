21d60c2fe29de1a9b463c4998c2da6c52991549c
Andrew Mary Huet de Barochez <andrewmhdb@gmail.com>
2022-01-12 18:22:33 +0100

    Ajout d'un mode d'affichage avec des emoji

--- src/Our_Board.py
+++ src/Our_Board.py
@@ -2,32 +2,38 @@ import chess
 
 
 class Our_Board:
-    
-    
     """
     Gère l'affichage dans le terminal
     """
-    
-    
-    
-    def __init__(self):
-        
+
+    def __init__(self, emojiMode=True):
         self.board: chess.Board = chess.Board()
-        
+        self.emojiMode = emojiMode
+
+    def get_base_display(self):
+        res = str(self.board)
+        if self.emojiMode:
+            res = res.replace("R", "♜")
+            res = res.replace("B", "♝")
+            res = res.replace("Q", "♛")
+            res = res.replace("K", "♚")
+            res = res.replace("P", "♟️")
+            res = res.replace("N", "♞")
+            res = res.replace("r", "♖")
+            res = res.replace("b", "♗")
+            res = res.replace("q", "♕")
+            res = res.replace("k", "♔")
+            res = res.replace("p", "♙")
+            res = res.replace("n", "️♘")
+            return res
+        else:
+            return res
 
     def display_board(self):
-        
-        res='  a b c d e f g h \n'
-        #x=self.board.__repr__()
-        x=str(self.board).split('\n')    
-        
-        
-        for i in range(1,9):
-            res+=f'{i} {x[i-1]}\n'
-            
-            
+        res = '  a b c d e f g h \n'
+        x = self.get_base_display().split('\n')
+
+        for i in range(1, 9):
+            res += f'{i} {x[i - 1]}\n'
+
         return res
-            
-        
-        
-        
