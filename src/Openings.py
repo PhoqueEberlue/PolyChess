5430ac781f30e2f750c6a0c335366f1114f535f8
Andrew Mary Huet de Barochez <andrewmhdb@gmail.com>
2022-01-13 09:55:31 +0100

    Ajout d'une fonction pour récuperer les ouvertures

--- src/Openings.py
+++ src/Openings.py
@@ -1,3 +1,15 @@
+import chess
+import chess.polyglot
+
+
 class Openings:
     def __init__(self):
-        pass
+        self.filename = "openings/book.bin"
+
+    def get_openings(self, board):
+        with chess.polyglot.open_reader(self.filename) as reader:
+            return [entry.move.uci() for entry in reader.find_all(board)]
+
+
+
+
